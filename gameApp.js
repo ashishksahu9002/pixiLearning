926ce97f8b5362cae71055eb497a29b167feedbb
Ashish Kumar <ashishksahu9002@gmail.com>
2025-09-13 23:39:34 +0530

    Containers & Transforms

--- gameApp.js
+++ gameApp.js
@@ -20,11 +20,46 @@ PIXI.Assets.load("./assets/bunny.png").then((texture) => {
 });
 
 function createBunny() {
-  const bunnySprite = new PIXI.Sprite(bunnyTexture);
+  const group = new PIXI.Container();
+  group.name = "group";
+  group.x = app.screen.width / 2;
+  group.y = app.screen.height / 2;
+  rootContainer.addChild(group);
 
-  bunnySprite.anchor.set(0.5);
-  bunnySprite.x = app.screen.width / 2;
-  bunnySprite.y = app.screen.height / 2;
+  for (let i = 0; i < 3; i++) {
+    const bunny = new PIXI.Sprite(bunnyTexture);
+    bunny.anchor.set(0.5);
+    bunny.name = `bunny${i + 1}`;
+    bunny.x = (i - 1) * 120; // positions: -120, 0, +120
+    bunny.y = 0;
 
-  rootContainer.addChild(bunnySprite);
+    bunny.eventMode = "static";
+    bunny.cursor = "pointer";
+
+    bunny.on("pointerenter", () => {
+      group.scale.set(1.2);
+    });
+
+    bunny.on("pointerleave", () => {
+      group.scale.set(1);
+    });
+
+    group.addChild(bunny);
+  }
+
+  const rotationText = new PIXI.Text("Rotation : 0°", {
+    fontSize: 30,
+    fill: 0xffffff,
+  });
+
+  rotationText.x = 10;
+  rotationText.y = 10;
+
+  rootContainer.addChild(rotationText);
+
+  app.ticker.add((delta) => {
+    group.rotation += delta * 0.01;
+    const deg = ((group.rotation * 180) / Math.PI) % 360;
+    rotationText.text = `Rotation: ${deg.toFixed(2)}°`;
+  });
 }
